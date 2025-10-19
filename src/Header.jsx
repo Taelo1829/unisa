0874926e80138b89938ce69744b9bf84d952fa93
Taelo <taelo@izon.group>
2025-10-19 21:11:18 +0200

    header fixed

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,13 @@ const Header = () => {
     return (
         <div className='header'>
             <div>
-                <h1>Exam Prep for COS1521</h1>
+                <img
+                    src='https://mymodules.dtls.unisa.ac.za/pluginfile.php/1/core_admin/logo/0x200/1760738292/myUnisa-logo.png'
+                    alt='unisa image'
+                    height={40}
+                />
             </div>
+           
             <div>
                 <div className='profile'>
                     <h2>TS</h2>
